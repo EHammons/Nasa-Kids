40b6889569b034eef46dfff892785609ea92ef22
Stardust <52759497+EHammons@users.noreply.github.com>
2020-01-23 20:37:09 -0600

    tweaking 404 page

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Col, Row, Container } from "../components/Grid";
-import Construction from "./construction-moon.png"
+import Construction from "./construction-moon.png";
+import "./style.css";
 
 function NoMatch() {
   return (
